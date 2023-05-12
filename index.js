5bf32777944e24eb151eca4ce36500b95e4fac3d
Abdul <abdul.rehmtulla@paypal.com>
2023-05-12 10:58:41 +0300

    Added fetching latest pull request

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Octokit } = require("octokit");
+const axios = require("axios");
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+//fetch env variables
+const [owner, repo] = process.argv.slice(2);
+
+async function fetchRepo() {
+  //fetch diff url from latest pull request
+  console.log("Fetching latest pull request...");
+  let diffUrl = "";
+  try {
+    diffUrl = await octokit
+      .request("GET /repos/{owner}/{repo}/pulls", {
+        owner: "bijuice",
+        repo: "portfolio-v2",
+      })
+      .then((res) => res.data[0].diff_url);
+  } catch (error) {
+    console.error("Unable to fetch repo");
+  }
+}
+
+fetchRepo();
