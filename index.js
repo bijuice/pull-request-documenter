85bafeb47e4dd828b0efd613d7cbb010654d164e
Abdul <abdul.rehmtulla@paypal.com>
2023-05-12 11:25:41 +0300

    Added explanation generation

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const { Octokit } = require("octokit");
 const axios = require("axios");
+const { Configuration, OpenAIApi } = require("openai");
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
+  auth: GITHUB_TOKEN,
 });
 
-//fetch env variables
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+//fetch command line params
 const [owner, repo] = process.argv.slice(2);
 
 async function fetchRepo() {
+  if (!GITHUB_TOKEN) {
+    console.error("GITHUB_TOKEN not found");
+    return;
+  }
+
+  if (!OPENAI_API_KEY) {
+    console.error("OPENAI_API_KEY not found");
+    return;
+  }
+
+  if (!owner || !repo) {
+    console.error(
+      "Please provide owner and repo in command line arguments. See README.md for more info. "
+    );
+    return;
+  }
+
   //fetch diff url from latest pull request
   console.log("Fetching latest pull request...");
   let diffUrl = "";
@@ -21,6 +49,7 @@ async function fetchRepo() {
       .then((res) => res.data[0].diff_url);
   } catch (error) {
     console.error("Unable to fetch repo");
+    return;
   }
 
   //fetch diff
@@ -28,11 +57,36 @@ async function fetchRepo() {
   let diff = "";
   try {
     diff = await axios.get(diffUrl).then((res) => res.data);
-
-    console.log(diff);
   } catch (error) {
     console.error(error);
+    return;
+  }
+
+  //generate explanation
+  console.log("Generating explanation...");
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(diff),
+      temperature: 0.6,
+    });
+
+    console.log(completion.data.choices);
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      return;
+    }
+
+    console.error(`Error with OpenAI API request: ${error.message}`);
+    return;
   }
 }
 
-// fetchRepo();
+function generatePrompt(diff) {
+  const prompt = `This is the diff for a pull request. I want you to explain the changes in plain english without explaining what the code does.\n ${diff} \n`;
+
+  return prompt;
+}
+
+fetchRepo();
